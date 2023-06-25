fa574b024fab25dae2277de05dfe4069f1cd728f
D3athstep <ibrahimkhalilullah799@gmail.com>
2023-06-25 23:05:19 +0500

    Inital commit

---
+++ Assets/Scripts/Shop/CloseButton_shop.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloseButton_shop : MonoBehaviour
+{
+
+    [Header("Shop Event")]
+    [SerializeField] GameObject shopUI;
+    [SerializeField] Button openShopButton;
+    [SerializeField]Button closeShopButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddShopEvents();
+    }
+
+    void AddShopEvents ()
+    {
+        openShopButton.onClick.RemoveAllListeners();
+        openShopButton.onClick.AddListener(OpenShop);
+
+        closeShopButton.onClick.RemoveAllListeners();
+        closeShopButton.onClick.AddListener(CloseShop);
+    }
+
+    void OpenShop ()
+    {
+        shopUI.SetActive(true);
+    }
+
+    void CloseShop()
+    {
+        shopUI.SetActive(false);
+    
+    
+    }
+
+}
+
